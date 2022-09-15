9c59c346d7dd4591335106f2dea1d8844130c648
yuvrajs <yuvraj1510@gmail.com>
2022-09-15 18:14:08 +0530

    [yuvrajs]Intializing the repository

---
+++ src/main/java/com/ysingh/javaspringaop/JavaSpringAopApplication.java
@@ -0,0 +1,13 @@
+package com.ysingh.javaspringaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringAopApplication.class, args);
+	}
+
+}
